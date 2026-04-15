50407b8c4583752c57d0a17350ebf95f4b28b33c
marquin <marcosvinicius11532@gmail.com>
2026-04-14 21:12:35 -0300

    Começo do projeto de filas
    Camada domain iniciada

---
+++ Projeto-Pay/Domain/Entidades/User.cs
@@ -0,0 +1,19 @@
+﻿using Domain.ObjetosValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entidades
+{
+    public class User
+    {
+        private Guid _id;
+
+        private ValueName _name;
+
+        private ValueEmail _email;
+
+        private ValuePassword _password;
+
+    }
+}
---
+++ Projeto-Pay/Domain/ObjetosValor/ValueEmail.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.ObjetosValor
+{
+    internal class ValueEmail
+    {
+    }
+}
